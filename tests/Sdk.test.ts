d7d4397d4a48806d95e24f75d04d614fa7ccde81
pierricklefebvre-paygreen <pierrick.lefebvre@paygreen.fr>
2020-06-22 17:42:32 +0200

    feat: First test added to paygreen-node (Test of SDK's initialization feat: Configuration file added in tests folder

---
+++ tests/Sdk.test.ts
@@ -0,0 +1,27 @@
+require('dotenv').config('/.env');
+import { Sdk } from '../src/Sdk';
+import { Mode } from '../src/enums/Mode';
+
+const config = {
+    shopId: process.env.SDK_SHOPID,
+    privateKey: process.env.SDK_KEY,
+    mode: Mode.PREPROD,
+};
+const sdk = new Sdk(config);
+
+test('Sdk has properties _tokens & _identity', () => {
+    expect(sdk).toHaveProperty('_key');
+    expect(sdk).toHaveProperty('_identity');
+});
+
+test('Sdk constructor initializes correctly with all the parameters', () => {
+    expect(sdk.shopId).toBe(process.env.SDK_SHOPID);
+    expect(sdk.privateKey).toBe(process.env.SDK_KEY);
+    expect(sdk.mode).toEqual(0);
+});
+
+const sdk2 = new Sdk();
+
+test('Sdk constructor initializes correctly with default value for mode', () => {
+    expect(sdk2.mode).toEqual(2);
+});
