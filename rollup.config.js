7fa628188236c17ff3bcdb6259d943d39ce1ac55
pierricklefebvre-paygreen <pierrick.lefebvre@paygreen.fr>
2020-07-02 14:26:43 +0200

    feat: New rollup plugin added => rollup-plugin-typescript2 feat: Target module modified (CommonJS -> ES2015) in tsconfig.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,37 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
-import pkg from "./package.json";
-import { terser } from "rollup-plugin-terser";
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import pkg from './package.json';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
 
-const extensions = [".js", ".jsx", ".ts", ".tsx"];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default {
-  input: "src/index.ts",
-  external: [...Object.keys(pkg.dependencies || {})],
-  plugins: [
-    resolve({ extensions }),
-    commonjs(),
-    babel({ extensions, include: ["src/**/*"] }),
-    terser({
-      mangle: {
-        keep_fnames: true,
-        keep_classnames: true
-      }
-    })
-  ],
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs"
-    },
-    {
-      file: pkg.module,
-      format: "es"
-    }
-  ]
+    input: 'src/index.ts',
+    external: [...Object.keys(pkg.dependencies || {})],
+    plugins: [
+        resolve({ extensions }),
+        commonjs(),
+        typescript({
+            typescript: require('typescript'),
+        }),
+        babel({ extensions, include: ['src/**/*'] }),
+        terser({
+            mangle: {
+                keep_fnames: true,
+                keep_classnames: true,
+            },
+        }),
+    ],
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+    ],
 };
